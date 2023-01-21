ffa2fc1b8f6a1d6c415c402fa16af973415659e1
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2023-01-21 22:23:43 +0530

    define all strings of sprint1

--- lib/app/common_widget/no_data_found_widget.dart
+++ lib/app/common_widget/no_data_found_widget.dart
@@ -2,14 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:foodmood/app/common_widget/text_widget.dart';
 import 'package:foodmood/app/res/strings/strings.dart';
 
-
 class NoDataFound extends StatelessWidget {
   const NoDataFound({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: TextWidget(text: AppStrings.noDataFound),
+      child: TextWidget(text: 'No data found'),
     );
   }
 }
--- lib/app/res/strings/strings.dart
+++ lib/app/res/strings/strings.dart
@@ -3,29 +3,16 @@ class AppStrings {
   AppStrings._();
 
   /*-------------------General Strings-------------------*/
-  static const String appName = 'OPS';
+
   static const String submit = 'Submit';
-  static const String myJobs = 'My Jobs';
-  static const String editProfile = 'Edit Profile';
-  static const String chatRoom = 'Chat Room';
-  static const String chatRooms = 'Chat Rooms';
   static const String name = 'Name';
   static const String enterName = 'Enter name';
   static const String enterLocation = 'Enter location';
-  static const String virtualDiary = 'Virtual Diary';
   static const String pending = 'Pending';
   static const String inProgress = 'InProgress';
   static const String completed = 'Completed';
-  static const String updateDocket = 'Update Docket';
-  static const String newDocket = 'New Docket';
-  static const String txtSomethingWentWrong = 'Something went wrong!';
-  static const String txtInvalidJobId = 'Invalid job id!';
-  static const String jobDetails = 'Job Details';
-  static const String noDataFound = 'No Jobs found';
-  static const String announcementsNotFound = 'Announcements not found!';
   static const String date = 'Date: ';
   static const String docketNo = 'Docket No. ';
-  static const String admin = 'Admin: ';
   static const String save = 'Save';
   static const String delete = 'Delete';
   static const String editItem = 'Edit Item';
@@ -56,14 +43,7 @@ class AppStrings {
   static const String logout = 'Logout';
   static const String yes = 'Yes';
   static const String no = 'No';
-  static const String noConversations = 'No Conversations';
-  static const String distanceTraveled = 'Distance traveled for the job ';
-  static const String expensesLinkedWithJob = 'Expenses linked with the job';
-  static const String youAreBlocked =
-      'You are blocked from sending messages in this chat room!';
-  static const String youAreRemoved = 'You are removed from this chat room!';
-  static const String addVirtualDiary = 'Add Virtual Diary';
-  static const String editVirtualDiary = 'Edit Virtual Diary';
+
   static const String description = 'Description';
   static const String enterDescription = 'Enter description';
   static const String amount = 'Amount';
@@ -119,4 +99,21 @@ class AppStrings {
   static const String txtPleaseSelectImage =
       'Please select image from the options below.';
   static const String txtGalleryPermission = 'Gallery Permission';
+
+  //Sprint 1 Strings
+  static const String onboardingTitle1 = 'All your favourites restaurants';
+  static const String onboardingTitle2 = 'Unmatched reliability';
+  static const String onboardingTitle3 = '24/7 support for you';
+  static const String onboardingTitle4 = 'Eat it in your own cozy comfort';
+
+  static const String onboardingDesc1 =
+      'Order from the best local restaurants with ease and on-demand delivery';
+  static const String onboardingDesc2 =
+      'Experience peace of mind while tracking your order in real time';
+  static const String onboardingDesc3 =
+      'Something came up? Talk to a real person. We are here to help';
+  static const String onboardingDesc4 =
+      'Want a food of your mood? Get your tummies filled with the edible good';
+
+  static const String getMoody = 'Get Moody';
 }
