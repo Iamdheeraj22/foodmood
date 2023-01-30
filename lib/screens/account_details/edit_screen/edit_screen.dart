b60f5166006c6b8d4ab8ef2e4f5d219b20d7449f
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2023-01-30 23:04:49 +0530

    Customise the UI & functionality of the editext in EditPersonal Details Screen

--- lib/screens/account_details/edit_screen/edit_screen.dart
+++ lib/screens/account_details/edit_screen/edit_screen.dart
@@ -38,83 +38,85 @@ class _EditScreenPageState extends State<EditScreenPage> {
       ),
       body: Container(
         margin: EdgeInsets.symmetric(horizontal: 20.r),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 25.h,
-            ),
-            //Edit Profile Picture
-            Center(
-              child: Container(
-                padding: EdgeInsets.all(15.r),
-                height: 175.h,
-                width: 175.h,
-                child: Stack(
-                  alignment: Alignment.bottomRight,
-                  children: [
-                    Container(
-                      height: 150.h,
-                      width: 150.h,
-                      decoration: BoxDecoration(
-                          color: context.appPrimaryColor,
-                          borderRadius: BorderRadius.circular(100.r)),
-                      child: Icon(
-                        Icons.person,
-                        size: 50.h,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 25.h,
+              ),
+              //Edit Profile Picture
+              Center(
+                child: Container(
+                  padding: EdgeInsets.all(15.r),
+                  height: 175.h,
+                  width: 175.h,
+                  child: Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      Container(
+                        height: 150.h,
+                        width: 150.h,
+                        decoration: BoxDecoration(
+                            color: context.appPrimaryColor,
+                            borderRadius: BorderRadius.circular(100.r)),
+                        child: Icon(
+                          Icons.person,
+                          size: 50.h,
+                        ),
                       ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(10.r),
-                      height: 48.h,
-                      width: 48.h,
-                      decoration: BoxDecoration(
-                          color: context.black,
-                          borderRadius: BorderRadius.circular(50.r)),
-                      child: Image.asset(
-                        AppIcons.camera,
-                      ),
-                    )
-                  ],
+                      Container(
+                        padding: EdgeInsets.all(10.r),
+                        height: 48.h,
+                        width: 48.h,
+                        decoration: BoxDecoration(
+                            color: context.black,
+                            borderRadius: BorderRadius.circular(50.r)),
+                        child: Image.asset(
+                          AppIcons.camera,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 50.h,
-            ),
-            CustomEditTextWidget(
-              title: Strings.name,
-              hintText: Strings.enterName,
-              controller: _nameController,
-              inputAction: TextInputAction.next,
-              inputType: TextInputType.text,
-              update: () {},
-              cancel: () {},
-            ),
-            SizedBox(
-              height: 25.h,
-            ),
-            CustomEditTextWidget(
-              update: () {},
-              cancel: () {},
-              title: Strings.phoneNumber,
-              hintText: Strings.enterPhoneNumber,
-              controller: _phoneController,
-              inputAction: TextInputAction.next,
-              inputType: TextInputType.phone,
-            ),
-            SizedBox(
-              height: 25.h,
-            ),
-            CustomEditTextWidget(
-              update: () {},
-              cancel: () {},
-              title: Strings.email,
-              hintText: Strings.enterEmailHint,
-              controller: _emailController,
-              inputAction: TextInputAction.done,
-              inputType: TextInputType.emailAddress,
-            )
-          ],
+              SizedBox(
+                height: 50.h,
+              ),
+              CustomEditTextWidget(
+                title: Strings.name,
+                hintText: Strings.enterName,
+                controller: _nameController,
+                inputAction: TextInputAction.next,
+                inputType: TextInputType.text,
+                update: () {},
+                cancel: () {},
+              ),
+              SizedBox(
+                height: 25.h,
+              ),
+              CustomEditTextWidget(
+                update: () {},
+                cancel: () {},
+                title: Strings.phoneNumber,
+                hintText: Strings.enterPhoneNumber,
+                controller: _phoneController,
+                inputAction: TextInputAction.next,
+                inputType: TextInputType.phone,
+              ),
+              SizedBox(
+                height: 25.h,
+              ),
+              CustomEditTextWidget(
+                update: () {},
+                cancel: () {},
+                title: Strings.email,
+                hintText: Strings.enterEmailHint,
+                controller: _emailController,
+                inputAction: TextInputAction.done,
+                inputType: TextInputType.emailAddress,
+              )
+            ],
+          ),
         ),
       ),
     );
