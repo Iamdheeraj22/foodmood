5b732fdf51e8bde790751550cdbcf79f1fbda323
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2023-02-03 23:01:54 +0530

    Designing completed & Customise the cart screen

--- lib/screens/cart/screens/cart_screen.dart
+++ lib/screens/cart/screens/cart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodmood/app/common_widget/text_widget.dart';
 import 'package:foodmood/app/res/colors/colors.dart';
 import 'package:foodmood/app/res/size/size_config.dart';
 import 'package:foodmood/screens/cart/widgets/cart_item_card.dart';
@@ -25,11 +26,63 @@ class _CartScreenState extends State<CartScreen> {
         ),
         centerTitle: true,
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: Column(
-          children: [CartItemCard()],
-        ),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.w),
+            child: Column(
+              children: [const CartItemCard(), const CartItemCard()],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(color: context.appPrimaryColor),
+            padding: EdgeInsets.symmetric(
+              horizontal: 10.w,
+              vertical: 10.h,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                RichText(
+                    text: TextSpan(children: [
+                  TextSpan(
+                      text: '₹ 175',
+                      style: TextStyle(
+                          fontSize: 24.sp, fontWeight: FontWeight.w500)),
+                  TextSpan(
+                      text: '\nIncludes all GST & Taxes',
+                      style: TextStyle(
+                          fontSize: 16.sp, fontWeight: FontWeight.w500)),
+                ])),
+                SizedBox(
+                  height: 20.h,
+                  width: 20.h,
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.info,
+                        color: context.white,
+                      )),
+                ),
+                const Spacer(),
+                Card(
+                  child: Container(
+                      alignment: Alignment.center,
+                      height: 50.h,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 20.w,
+                      ),
+                      child: TextWidget(
+                        text: 'Checkout',
+                        fontWeight: FontWeight.w400,
+                        textSize: 18.sp,
+                      )),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/screens/cart/widgets/cart_item_card.dart
+++ lib/screens/cart/widgets/cart_item_card.dart
@@ -10,84 +10,104 @@ class CartItemCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Row(
+      elevation: 3.r,
+      child: Stack(
+        alignment: Alignment.topRight,
         children: [
-          Image.asset(
-            FoodStrings.paneer,
-            height: 200.h,
-            width: 200.h,
-          ),
-          SizedBox(
-            width: 15.w,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Row(
             children: [
-              TextWidget(
-                text: 'Loaded Pizza',
-                fontWeight: FontWeight.w800,
-                textSize: 24.sp,
-                color: context.appPrimaryColor,
-              ),
-              TextWidget(
-                text: 'Regular',
-                fontWeight: FontWeight.w400,
-                textSize: 20.sp,
-                color: context.black,
+              ClipRRect(
+                borderRadius: BorderRadius.circular(5.r),
+                child: Image.asset(
+                  FoodStrings.paneer,
+                  height: 200.h,
+                  width: 200.h,
+                ),
               ),
               SizedBox(
-                height: 15.h,
+                width: 15.w,
               ),
-              TextWidget(
-                text: '₹ 175',
-                fontWeight: FontWeight.w700,
-                textSize: 28.sp,
-                color: context.black,
-              ),
-              SizedBox(
-                height: 10.h,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  TextWidget(
+                    text: 'Loaded Pizza',
+                    fontWeight: FontWeight.w800,
+                    textSize: 24.sp,
+                    color: context.appPrimaryColor,
+                  ),
+                  TextWidget(
+                    text: 'Regular',
+                    fontWeight: FontWeight.w400,
+                    textSize: 20.sp,
+                    color: context.black,
+                  ),
                   SizedBox(
-                    height: 50.h,
-                    width: 50.w,
-                    child: FloatingActionButton(
-                      onPressed: () {},
-                      backgroundColor: context.appPrimaryColor,
-                      child: TextWidget(
-                        text: '-',
-                        textSize: 20.sp,
-                        fontWeight: FontWeight.w600,
+                    height: 15.h,
+                  ),
+                  TextWidget(
+                    text: '₹ 175',
+                    fontWeight: FontWeight.w700,
+                    textSize: 28.sp,
+                    color: context.black,
+                  ),
+                  SizedBox(
+                    height: 10.h,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 50.h,
+                        width: 50.w,
+                        child: FloatingActionButton(
+                          onPressed: () {},
+                          backgroundColor: context.appPrimaryColor,
+                          child: TextWidget(
+                            text: '-',
+                            textSize: 20.sp,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 15.w,
-                  ),
-                  TextWidget(text: '1'),
-                  SizedBox(
-                    width: 15.w,
-                  ),
-                  SizedBox(
-                    height: 50.h,
-                    width: 50.w,
-                    child: FloatingActionButton(
-                      onPressed: () {},
-                      backgroundColor: context.appPrimaryColor,
-                      child: TextWidget(
-                        text: '+',
-                        textSize: 20.sp,
-                        fontWeight: FontWeight.w600,
+                      SizedBox(
+                        width: 15.w,
                       ),
-                    ),
-                  ),
+                      TextWidget(text: '1'),
+                      SizedBox(
+                        width: 15.w,
+                      ),
+                      SizedBox(
+                        height: 50.h,
+                        width: 50.w,
+                        child: FloatingActionButton(
+                          onPressed: () {},
+                          backgroundColor: context.appPrimaryColor,
+                          child: TextWidget(
+                            text: '+',
+                            textSize: 20.sp,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
                 ],
               )
             ],
-          )
+          ),
+          SizedBox(
+            height: 30.h,
+            width: 30.w,
+            child: FloatingActionButton(
+                onPressed: () {},
+                backgroundColor: context.appPrimaryColor,
+                child: Icon(
+                  Icons.close,
+                  size: 20.r,
+                )),
+          ),
         ],
       ),
     );
